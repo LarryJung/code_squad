2e66f6e3209e6c13e4130466a9835feb28e53c85
larryjung <corbswjd@gmail.com>
2018-02-19 23:00:57 +0900

    계산기 루프 구현, not symbol 예외처리

--- src/java_playground/arithcalculator/StringCalculator.java
+++ src/java_playground/arithcalculator/StringCalculator.java
@@ -1,57 +1,49 @@
 package java_playground.arithcalculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
-
 
 public class StringCalculator {
 	private static Scanner sc;
 
 	public static void main(String[] args) {
-		
+
 		sc = new Scanner(System.in);
 		System.out.println("fitst number : ");
-		String first = sc.next();
-		int result = Integer.parseInt(first);
+		int first = sc.nextInt();
+		int result = first;
 		while (true) {
+			String[] symbols = new String[]{"+", "-", "*", "/"};
 			System.out.println("기호 : ");
 			String symbol = sc.next();
+			boolean pass = Arrays.stream(symbols).anyMatch(symbol::equals);
 			
 			if (symbol.equals("quit")) {
 				System.out.println("result : " + result);
 				break;
-			} else if (symbol.equals("+")){
+			} else if (pass) {
 				System.out.println("second number : ");
-				String second = sc.next();
-				int sec = Integer.parseInt(second);
-				result = result + sec;
-				System.out.println("result : " + result);
-			} else if (symbol.equals("+")){
-				System.out.println("second number : ");
-				String second = sc.next();
-				int sec = Integer.parseInt(second);
-				result = result + sec;
-				System.out.println("result : " + result);
-			} else if (symbol.equals("-")){
-				System.out.println("second number : ");
-				String second = sc.next();
-				int sec = Integer.parseInt(second);
-				result = result - sec;
-				System.out.println("result : " + result);
-			} else if (symbol.equals("*")){
-				System.out.println("second number : ");
-				String second = sc.next();
-				int sec = Integer.parseInt(second);
-				result = result * sec;
-				System.out.println("result : " + result);
-			} else if (symbol.equals("/")){
-				System.out.println("second number : ");
-				String second = sc.next();
-				int sec = Integer.parseInt(second);
-				result = result / sec;
-				System.out.println("result : " + result);
+				int second = sc.nextInt();
+
+				if (symbol.equals("+")) {
+					result = result + second;
+					System.out.println("result : " + result);
+				} else if (symbol.equals("+")) {
+					result = result + second;
+					System.out.println("result : " + result);
+				} else if (symbol.equals("-")) {
+					result = result - second;
+					System.out.println("result : " + result);
+				} else if (symbol.equals("*")) {
+					result = result * second;
+					System.out.println("result : " + result);
+				} else if (symbol.equals("/")) {
+					result = result / second;
+					System.out.println("result : " + result);
+				} 
 			} else {
 				System.out.println("not a symbol");
+				}
 			}
 		}
-	}
 }
